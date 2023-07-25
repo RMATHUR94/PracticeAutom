e91f28124e91ceae49da1c066c9158cf3facfee6
Rahul <rahul.tisl001@gmail.com>
2023-07-25 12:41:55 +0530

    data_provider

--- Naveenautomtestng/src/test/java/com/test/utility/TestUtil.java
+++ Naveenautomtestng/src/test/java/com/test/utility/TestUtil.java
@@ -1,31 +1,50 @@
 package com.test.utility;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Calendar;
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.eventusermodel.XSSFReader;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFCreationHelper;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import com.excel.utility.Xls_Reader;
-
 public class TestUtil {
-	
+
+static XSSFReader reader;
+
   public static ArrayList<Object[]> getDataFromExcel() throws IOException
   {
 	  
 	  ArrayList<Object[]> myData = new ArrayList<Object[]>();
 	  
-	  FileInputStream fis = new FileInputStream("/home/vyrazu-66/Documents/Syseclipse/Naveenautomtestng/src/test/java/com/testdata/testdata.xlsx");
-	  XSSFWorkbook workbook = new XSSFWorkbook(fis);
-	  XSSFSheet sheet = workbook.getSheetAt(0);
+	  reader = new XSSFReader("/home/vyrazu-66/Documents/Syseclipse/Naveenautomtestng/src/test/java/com/testdata/testdata.xlsx");
 	  
-	  for(int rowNum = 2 ; rowNum <= sheet.; rowNum ++)
+	  
+	  
+//	  
+//	  FileInputStream fis = new FileInputStream("/home/vyrazu-66/Documents/Syseclipse/Naveenautomtestng/src/test/java/com/testdata/testdata.xlsx");
+//	  XSSFWorkbook workbook = new XSSFWorkbook(fis);
+//	  XSSFSheet sheet = workbook.getSheetAt(0);
+	  
+	  for(int rowNum = 2 ; rowNum <= reader.getRowCount("RegTestData"); rowNum ++)
 	  {
+		  String firstname = reader.getCellData("RegTestData" , "firstname",rowNum);
+		  String lastname = reader.getCellData("RegTestData" , "lastname",rowNum);
+		  String email = reader.getCellData("RegTestData" , "email",rowNum);
+		  int contactno = reader.getCellData("RegTestData" , "contactno",rowNum);
+		  String password =reader.getCellData("RegTestData" , "password",rowNum);
+		  String confirm_password = reader.getCellData("RegTestData" , "confirm_password",rowNum);
 		  
+		  Object ob[] = {firstname ,lastname,email,contactno,password,confirm_password};
+		  myData.add(ob);
 	  }
+	  return myData;
    }
-    return myData;
+   
 }
